5b281f13ba2eba78ae9a85feceb864296b1d079d
gnswp21 <gnswp21@gmail.com>
2024-03-06 23:22:38 +0900

    ♻️ refactor remove resource manager node

--- docker-build.cmd
+++ docker-build.cmd
@@ -4,5 +4,4 @@ docker build -t language-base ./language-base && ^
 docker build -t hadoop-base ./hadoopbase && ^
 docker build -t hadoop-namenode ./namenode && ^
 docker build -t hadoop-datanode ./datanode && ^
-docker build -t resourcemanager ./resourcemanager && ^
 docker-compose up -d
--- resourcemanager/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#$HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR resourcemanager
-service ssh start && tail -f /dev/null
